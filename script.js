2d831a747cef40380abf06782923d0f687f59de5
MostafaAmrHamed <chetosamr@gmail.com>
2019-09-28 23:06:12 +0200

    Comming Soon..

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function Charx()
     x= String.fromCharCode( Math.floor(Math.random() * 26) + 97);
     document.getElementById("test").innerHTML+=x; 
 }
-setInterval(function(){ Charx();}, 500);
+setInterval(function(){ Charx();}, 1000);
 
 
 
