5960b5d9a86cb0379b5afa35ff1ddaa0cc252d94
MostafaAmrHamed <chetosamr@gmail.com>
2019-09-29 02:19:51 +0200

    Under Maintance xD

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 var char;
-var x ;
+var x = new Array();
+var count = 0;
 
 function Charx()
 {
     x= String.fromCharCode( Math.floor(Math.random() * 26) + 97);
-    document.getElementById("test").innerHTML+=x; 
+    document.getElementById("test").innerHTML+=x;   
 }
 setInterval(function(){ Charx();}, 2000);
 
@@ -21,13 +22,23 @@ $(function(){
 
 function Test()
 {
+    
     if(x[0]==char)
     {
-        document.getElementById("test2").innerHTML="True";
+        count+=1;
+        document.getElementById("test2").innerHTML=count;
+        x.shift();
     }
     else
     {
-        document.getElementById("test2").innerHTML="False";
+        count-=1;
+        document.getElementById("test2").innerHTML=count;
+       /*if(live = 0)
+        {
+            alert("Hide!");
+            document.getElementById( 
+                "test").style.visibility = "hidden"; 
+        }*/
     }
 }
 
