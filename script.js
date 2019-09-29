72a7b62670efb9372822d08a5627e313f5f71c77
MostafaAmrHamed <chetosamr@gmail.com>
2019-09-29 03:46:09 +0200

    90% Done

--- script.js
+++ script.js
@@ -9,31 +9,34 @@ function Charx()
 }
 
 
-
-
-
 $(function(){
     $(window).keypress((e)=>{
        var char = String.fromCharCode(e.which);
        var gameBoard = document.getElementById("gameBoard").innerHTML;
         if(gameBoard[0]==char)
     {
-        document.getElementById("test2").innerHTML= ++count;
+        document.getElementById("score").innerHTML= ++count;
         gameBoard = gameBoard.substring(1);
         document.getElementById("gameBoard").innerHTML= gameBoard;
     }
     else
     {
-        
-        lives = -1;
-        document.getElementById("test2").innerHTML= --count;
+        lives--;
+        if(lives <0)
+        {
+            clearInterval(t);
+            document.getElementById("score").innerHTML = "You Lost Your Score Was : " + count;
+            return;
+        }
+
+        if(count < 1)
+            return;
+        document.getElementById("score").innerHTML= --count;
     } 
     });
 });
-
-
-
-setInterval(function(){ Charx();}, 1000);
+ 
+var t = setInterval(function(){ Charx();}, 1000);
 
 
 
